aa128662bbfe9c427f58d2277fb51fa2370c2753
Yifei Sun <ysun@hey.com>
2023-12-30 17:40:35 -0500

    writes follow reads init

---
+++ consistency/model/writes_follow_reads.py
@@ -0,0 +1,17 @@
+import z3
+
+from consistency.abstract_execution import AbstractExecution
+from consistency.constraint import Constraint
+
+
+class WritesFollowReads:
+    @staticmethod
+    def constraints(s: z3.Solver) -> None:
+        _, (rd, wr) = Constraint.declare_operation_type()
+        op = Constraint.declare_operation()
+        ...
+
+
+    @staticmethod
+    def check(ae: AbstractExecution) -> bool:
+        ...

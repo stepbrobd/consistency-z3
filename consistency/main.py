65f92761cb1a0407ed6a488c058eb5f781d2eef9
Yifei Sun <ysun@hey.com>
2024-01-15 21:10:26 -0500

    simplify example

--- consistency/main.py
+++ consistency/main.py
@@ -12,6 +12,14 @@ from consistency.model.writes_follow_reads import WritesFollowReads
 
 
 def main() -> int:
+    models = [
+        PRAMConsistency,
+        MonotonicReads,
+        MonotonicWrites,
+        ReadYourWrites,
+        WritesFollowReads,
+    ]
+
     # note:
     # if a system S_1 provide guarantee G_1, another system S_2 provide guarantee G_2
     # compatible(G_1, G_2) == True means
@@ -20,41 +28,21 @@ def main() -> int:
 
     # compatibility is not symmetric
     # i.e. if mr is compatible with ryw, then ryw is not necessarily compatible with mr
-    for a, b in itertools.product([
-        # (PRAMConsistency, "pram"), # weirdly, pram seems to affect the result of other models
-        (MonotonicReads, "mr"),
-        (MonotonicWrites, "mw"),
-        (ReadYourWrites, "ryw"),
-        (WritesFollowReads, "wfr")
-    ], repeat=2):
-        if a == b:
+
+    # from the paper:
+    # > "As proved by Brzezinski et al. [2003], PRAM consistency is ensured iff the system provides read-your-write, monotonic reads and monotonic writes guarantee"
+    # meaning `compatible(pram, {ryw, mr, mw}) == True`
+
+    for lhs, rhs in itertools.product(models, repeat=2):
+        if lhs == rhs:
             continue
 
         s1 = z3.Solver()
         s2 = z3.Solver()
-        a[0].constraints(s1)
-        b[0].constraints(s2)
+        lhs.constraints(s1)
+        rhs.constraints(s2)
 
-        print(f"{a[1]} | {b[1]}: {compatible(s1, s2)}")
-
-        z3.reset_params()
-
-    # fix z3 assertion logic
-    # from the paper:
-    # > "As proved by Brzezinski et al. [2003], PRAM consistency is ensured iff the system provides read-your-write, monotonic reads and monotonic writes guarantee"
-    # meaning `compatible(pram, {ryw, mr, mw}) == True`
-    for m in [
-        (MonotonicReads, "mr"),
-        (MonotonicWrites, "mw"),
-        (ReadYourWrites, "ryw"),
-        (WritesFollowReads, "wfr"),
-    ]:
-        s1 = z3.Solver()
-        s2 = z3.Solver()
-        PRAMConsistency.constraints(s1)
-        m[0].constraints(s2)
-
-        print(f"pram | {m[1]}: {compatible(s1, s2)}")
+        print(f"{lhs.__name__} | {rhs.__name__}: {compatible(s1, s2)}")
 
         z3.reset_params()
 

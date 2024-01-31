ebf70b2cd02c2fe136623cd207e2604c3f0220c4
Yifei Sun <ysun@hey.com>
2024-01-30 22:05:52 -0500

    add hydra jobs

--- flake.nix
+++ flake.nix
@@ -13,47 +13,53 @@
     , nixpkgs
     , flake-utils
     , pyproject
-    }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      python = pkgs.python311;
-      project = pyproject.lib.project.loadPyproject { projectRoot = ./.; };
-    in
-    {
-      formatter = pkgs.writeShellScriptBin "formatter" ''
-        set -eoux pipefail
-        shopt -s globstar
-        ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt .
-        ${pkgs.ruff}/bin/ruff --fix --unsafe-fixes .
-        ${pkgs.typstfmt}/bin/typstfmt **/*.typ
-      '';
-
-      packages.default = python.pkgs.buildPythonPackage (
-        project.renderers.buildPythonPackage { inherit python; }
-      );
-
-      apps.default = flake-utils.lib.mkApp { drv = self.packages.${system}.default; };
-
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          direnv
-          git
-          hayagriva
-          nix-direnv
-          ruff
-          typst
-          typstfmt
-        ];
-
-        venvDir = "./.venv";
-        buildInputs = [ python ] ++ (with python.pkgs; [
-          venvShellHook
-          setuptools
-          wheel
-        ]);
-        postShellHook = ''
-          pip --disable-pip-version-check install -e .
+    }: flake-utils.lib.eachDefaultSystem
+      (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python311;
+        project = pyproject.lib.project.loadPyproject { projectRoot = ./.; };
+      in
+      {
+        formatter = pkgs.writeShellScriptBin "formatter" ''
+          set -eoux pipefail
+          shopt -s globstar
+          ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt .
+          ${pkgs.ruff}/bin/ruff --fix --unsafe-fixes .
+          ${pkgs.typstfmt}/bin/typstfmt **/*.typ
         '';
+
+        packages.default = python.pkgs.buildPythonPackage (
+          project.renderers.buildPythonPackage { inherit python; }
+        );
+
+        apps.default = flake-utils.lib.mkApp { drv = self.packages.${system}.default; };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            direnv
+            git
+            hayagriva
+            nix-direnv
+            ruff
+            typst
+            typstfmt
+          ];
+
+          venvDir = "./.venv";
+          buildInputs = [ python ] ++ (with python.pkgs; [
+            venvShellHook
+            setuptools
+            wheel
+          ]);
+          postShellHook = ''
+            pip --disable-pip-version-check install -e .
+          '';
+        };
+      }) // {
+      hydraJobs = {
+        inherit (self)
+          packages devShells;
       };
-    });
+    };
 }

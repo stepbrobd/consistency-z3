76cd8b20bea5595bad8f9b7973d9e69870eded9a
Yifei Sun <ysun@hey.com>
2024-02-04 20:28:29 -0500

    move common helpers

---
+++ consistency/common.py
@@ -0,0 +1,19 @@
+import z3
+
+from consistency.relation import Relation
+
+
+def check(assertions: z3.BoolRef) -> bool:
+    s = z3.Solver()
+    s.add([assertions, Relation.Constraints()])
+    return s.check() == z3.sat
+
+
+def compatible(lhs: z3.BoolRef, rhs: z3.BoolRef) -> bool:
+    s = z3.Solver()
+    s.add([z3.Not(z3.Implies(lhs, rhs)), Relation.Constraints()])
+    return s.check() == z3.unsat
+
+
+def compose(*assertions: z3.BoolRef) -> z3.BoolRef:
+    return z3.And(*assertions)

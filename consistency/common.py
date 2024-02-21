eb4a71dfdbc0458befd768f10aaad32711ed2df2
Yifei Sun <ysun@hey.com>
2024-02-20 20:04:05 -0500

    online store example (incomplete)

--- consistency/common.py
+++ consistency/common.py
@@ -3,15 +3,15 @@ import z3
 from consistency.relation import Relation
 
 
-def check(assertions: z3.BoolRef) -> bool:
+def check(assertions: z3.BoolRef, others: z3.AstRef = z3.BoolVal(True)) -> bool:
     s = z3.Solver()
-    s.add([assertions, Relation.Constraints()])
+    s.add([assertions, Relation.Constraints(), others])
     return s.check() == z3.sat
 
 
-def compatible(lhs: z3.BoolRef, rhs: z3.BoolRef) -> bool:
+def compatible(lhs: z3.BoolRef, rhs: z3.BoolRef, others: z3.AstRef = z3.BoolVal(True)) -> bool:
     s = z3.Solver()
-    s.add([z3.Not(z3.Implies(lhs, rhs)), Relation.Constraints()])
+    s.add([z3.Not(z3.Implies(lhs, rhs)), Relation.Constraints(), others])
     return s.check() == z3.unsat
 
 
--- tests/test_composition.py
+++ tests/test_composition.py
@@ -1,6 +1,10 @@
+import itertools
+
 import z3
 
+from consistency.abstract_execution import AbstractExecution as AE
 from consistency.common import compatible, compose
+from consistency.history import History as H
 from consistency.model.monotonic_reads import MonotonicReads
 from consistency.model.monotonic_writes import MonotonicWrites
 from consistency.model.pram_consistency import PRAMConsistency
@@ -49,48 +53,111 @@ def test_simple_online_shop() -> None:
     _, (rd, wr) = Op.Sort()
     op = Op.Create()
 
-    # clients to shopping cart (need to see their own writes):
+    H.Relation.returns_before()
+    H.Relation.same_session()
+    so = H.Relation.session_order()
+    vis = AE.Relation.visibility()
+    AE.Relation.arbitration()
+
+    # client:
     # - client reads shopping cart
     # - client writes shopping cart
     # - client writes shop
-    client_op = z3.Datatype("ClientOp")
-    client_op.declare("crc", ("op", op))
-    client_op.declare("cwc", ("op", op))
-    client_op.declare("cws", ("op", op))
-    client_op = client_op.create()
-
-    client = z3.Const("client", client_op)
-    client_constraints = [
-        z3.ForAll([client], z3.Implies(client_op.op(client) == client_op.crc, op.type(client_op.op(client)) == rd))
-        # z3.ForAll([client], z3.Implies(client_op.op(client) == client_op.cwc, op.type(client_op.op(client)) == wr)),
-        # z3.ForAll([client], z3.Implies(client_op.op(client) == client_op.cws, op.type(client_op.op(client)) == wr)),
-    ]
-
+    # cart:
+    # - cart write client
     # shop:
     # - shop reads inventory
     # - shop writes inventory
     # - shop writes transaction log
     # - shop reads transaction log
+
+    # client:
+    client_op_type, (crc, cwc, cws) = z3.EnumSort("ClientOpType", ["crc", "cwc", "cws"])
+    client_op = z3.Datatype("ClientOp")
+    client_op.declare("cons",
+        ("type", client_op_type),
+        ("op", op),
+    )
+    client_op = client_op.create()
+    cop = z3.Const("cop", client_op)
+    cop_a, cop_b = z3.Consts("cop_a cop_b", client_op)
+    cop_constraints = [
+        # base constraints
+        z3.Implies(client_op.type(cop) == crc, op.type(client_op.op(cop)) == rd),
+        z3.Implies(client_op.type(cop) == cwc, op.type(client_op.op(cop)) == wr),
+        z3.Implies(client_op.type(cop) == cws, op.type(client_op.op(cop)) == wr),
+    ]
+
+    # cart
+    cart_op_type, (kwc, ) = z3.EnumSort("CartOpType", ["kwc"])
+    cart_op = z3.Datatype("CartOp")
+    cart_op.declare("cons",
+        ("type", cart_op_type),
+        ("op", op),
+    )
+    cart_op = cart_op.create()
+    kop = z3.Const("kop", cart_op)
+    kop_a, kop_b = z3.Consts("kop_a kop_b", cart_op)
+    kop_constraints = [
+        # base constraints
+        z3.Implies(cart_op.type(kop) == kwc, op.type(cart_op.op(kop)) == wr),
+    ]
+
+    # shop
+    shop_op_type, (sri, swi, swt, srt) = z3.EnumSort("ShopOpType", ["sri", "swi", "swt", "srt"])
     shop_op = z3.Datatype("ShopOp")
-    shop_op.declare("sri", ("op", op))
-    shop_op.declare("swi", ("op", op))
-    shop_op.declare("swt", ("op", op))
-    shop_op.declare("srt", ("op", op))
+    shop_op.declare("cons",
+        ("type", shop_op_type),
+        ("op", op),
+    )
     shop_op = shop_op.create()
 
-    z3.Const("shop", shop_op)
-    shop_constraints = []
-
-    # entity constraints
-    entity_constraints = [
-        client_constraints,
-        shop_constraints,
+    sop = z3.Const("sop", shop_op)
+    sop_a, sop_b = z3.Consts("sop_a sop_b", shop_op)
+    sop_constraints = [
+        # base constraints
+        z3.Implies(shop_op.type(sop) == sri, op.type(shop_op.op(sop)) == rd),
+        z3.Implies(shop_op.type(sop) == swi, op.type(shop_op.op(sop)) == wr),
+        z3.Implies(shop_op.type(sop) == swt, op.type(shop_op.op(sop)) == wr),
+        z3.Implies(shop_op.type(sop) == srt, op.type(shop_op.op(sop)) == rd),
     ]
 
     # check individual constraints
-    for m in models:
-        for c in entity_constraints:
-            compatible(m.assertions(), c)
+    for c in [cop_constraints, kop_constraints, sop_constraints]:
+        s = z3.Solver()
+        s.add(c)
+        assert s.check() == z3.sat
+        for m in models:
+            s.push()
+            s.add(m.assertions())
+            assert s.check() == z3.sat
+            s.pop()
+
+    # cross dependencies
+    deps = [
+        # client -> cart
+        # a rd crc guarantees a subsequent wr kwc
+        z3.Implies(z3.And(client_op.type(cop_a) == crc, cart_op.type(kop_b) == kwc, vis(client_op.op(cop_a), cart_op.op(kop_b))), so(client_op.op(cop_a), cart_op.op(kop_b))),
+        # a wr cwc guarantees a subsequent wr kwc
+        z3.Implies(z3.And(client_op.type(cop_a) == cwc, cart_op.type(kop_b) == kwc, vis(client_op.op(cop_a), cart_op.op(kop_b))), so(client_op.op(cop_a), cart_op.op(kop_b))),
+        # cart -> client
+        # a wr kwc guarantees all subsequent read of carts to be visible
+        z3.Implies(z3.And(cart_op.type(kop_a) == kwc, client_op.type(cop_b) == crc, vis(cart_op.op(kop_a), client_op.op(cop_b))), vis(cart_op.op(kop_a), client_op.op(cop_b))),
+        # shop -> cart
+    ]
+
+    # sanity check
+    s = z3.Solver()
+    s.add(cop_constraints + kop_constraints + sop_constraints + deps)
+    assert s.check() == z3.sat
 
     # assert all available constraints on top of base constraints in turn
-    ...
+    # compatible(m.assertions(), c)
+    # itertools generate combinations for clients, carts, shops
+    all = compose(*cop_constraints, *kop_constraints, *sop_constraints, *deps)
+    for cm, km, sm in itertools.product(models, repeat=3):
+        print(f"client: {cm.__name__}; cart: {km.__name__}; shop: {sm.__name__}")
+        for a, b in itertools.permutations([cm, km, sm], 2):
+            print(f"  {a.__name__} <- {b.__name__}")
+            # assert compatible(a.assertions(), b.assertions(), others=all)
+            compatible(a.assertions(), b.assertions(), others=all)

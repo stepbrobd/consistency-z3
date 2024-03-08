0dc2add22dcb1802e25cd48a0d4059ef7d4ec794
Yifei Sun <ysun@hey.com>
2024-03-08 17:56:22 -0500

    add notes about monotonicity (read ops can only read the same/newer version of data when comparing with previous reads

--- consistency/model/monotonic_reads.py
+++ consistency/model/monotonic_reads.py
@@ -26,9 +26,25 @@ class MonotonicReads:
         vis = AE.Relation.visibility()
 
         # monotonic read
-        return z3.ForAll([a, b, c],
+        return z3.And(
+            # (a -vis-> b /\ b --so-> c) -> a -vis-> c
+            z3.ForAll([a, b, c],
                 z3.Implies(
                     z3.And(vis(a, b), so(b, c), op.type(b) == rd, op.type(c) == rd),
                     vis(a, c)
                 )
+            ),
+            # monotonically increase
+            # for example:
+            # |---wr(a)---|
+            #               |-------wr(b)-------|
+            #                |-rd(b)-| |-rd(a)-|
+            # a write operation writes a key k to value a
+            # after the op returns, a long write operation writes the key k to value b
+            # at the same time, two read operations read the key k
+            # the first one return b, but the second one return a
+            # this is a violation of monotonicity
+            # z3.ForAll([a, b],
+            #
+            # )
         )

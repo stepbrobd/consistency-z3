16130acd3075f723d215c237db8b6cc1ce64d1d1
Yifei Sun <ysun@hey.com>
2024-11-12 22:37:02 -0500

    add 'viewed' definition

--- consistency/abstract_execution.py
+++ consistency/abstract_execution.py
@@ -8,6 +8,52 @@ from consistency.relation import Relation as Rel
 class AbstractExecution:
     class Relation(Rel):
         @staticmethod
+        def viewed() -> z3.FuncDeclRef:
+            op = Op.Create()
+            viewed = AbstractExecution.Relation.Declare("viewed", op, op, z3.BoolSort())
+
+            _, (rd, wr) = Op.Sort()
+            a, b, c = Op.Consts("a b c")
+            vis = AbstractExecution.Relation.visibility()
+            AbstractExecution.Relation.AddConstraint("viewed", z3.And(
+                # write-read: if b has seen a, subsequent reads after b must see data as up-to-date as a
+                # this also applies to concurrent writes
+                z3.If(z3.And(op.type(a) == wr, op.type(b) == rd, op.obj(a) == op.obj(b)), z3.Implies(viewed(a, b),
+                    z3.If(z3.Or(
+                        # non-concurrent
+                        op.rtime(a) < op.stime(b),
+                        # concurrent
+                        # this only captures the case where a and b *MIGHT* be concurrent
+                        z3.And(op.stime(a) <= op.stime(b), op.rtime(a) >= op.rtime(a)),
+                        z3.And(op.stime(a) >= op.stime(b), op.rtime(a) <= op.rtime(b)),
+                        z3.And(op.stime(b) <= op.rtime(a), op.rtime(a) <= op.rtime(b)),
+                        z3.And(op.stime(b) <= op.stime(a), op.stime(a) <= op.rtime(b)),
+                    ),
+                    # then
+                    vis(a, b),
+                    # else
+                    z3.BoolVal(True)
+                )), z3.BoolVal(True)),
+                # read-read: b's value being returned must track the closest write that's visible to a
+                # c -> ... -> a -> b
+                # wr...rd...rd...rd
+                z3.If(z3.And(op.type(a) == rd, op.type(b) == rd, op.obj(a) == op.obj(b)), z3.Implies(viewed(a, b),
+                    z3.Exists([c], z3.And(
+                        op.type(c) == wr,
+                        vis(c, a),
+                        op.ival(c) == op.oval(b), op.obj(b) == op.obj(a),
+                    ))
+                ), z3.BoolVal(True)),
+                # acyclicity
+                z3.ForAll([a, b], z3.Implies(viewed(a, b), z3.Not(viewed(b, a)))),
+                # transitivity
+                z3.ForAll([a, b, c], z3.Implies(z3.And(viewed(a, b), viewed(b, c)), viewed(a, c))),
+            ))
+
+            return viewed
+
+
+        @staticmethod
         def visibility() -> z3.FuncDeclRef:
             op = Op.Create()
             vis = AbstractExecution.Relation.Declare("vis", op, op, z3.BoolSort())

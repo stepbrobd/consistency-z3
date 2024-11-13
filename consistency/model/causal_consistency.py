22d57b551ef8405054a392c948f489e2779abf6a
Yifei Sun <ysun@hey.com>
2024-11-12 22:40:47 -0500

    causal consistency should include session causlity with writes-into constraints

--- consistency/model/causal_consistency.py
+++ consistency/model/causal_consistency.py
@@ -1,8 +1,10 @@
 import z3
 
 from consistency.abstract_execution import AbstractExecution as AE
+from consistency.common import compose
 from consistency.history import History as H
 from consistency.model.model import Model
+from consistency.model.writes_follow_reads import WritesFollowReads
 from consistency.operation import Operation as Op
 
 
@@ -11,7 +13,11 @@ class CausalConsistency(Model):
     def assertions() -> z3.BoolRef:
         op = Op.Create()
         _, (rd, wr) = Op.Sort()
-        a, b = Op.Consts("a b")
+        a, b, c = Op.Consts("a b c")
+
+        so = H.Relation.session_order()
+        ar = AE.Relation.arbitration()
+        vis = AE.Relation.visibility()
 
         # writes-into: https://repository.gatech.edu/server/api/core/bitstreams/a083b72e-3e3d-4252-9f10-5ab71fa7f6c5/content
         wi = H.Relation.Declare("wi", op, op, z3.BoolSort())
@@ -19,21 +25,20 @@ class CausalConsistency(Model):
             z3.Implies(wi(a, b), z3.And(
                 # a must be a write, b must be a read
                 # a and b must be wr/rd on the same object (shared memory location)
-                z3.If(op.type(a) == wr, z3.And(op.type(b) == rd, op.obj(a) == op.obj(b)), z3.BoolVal(True)),
+                z3.If(op.type(a) == wr, z3.And(op.type(b) == rd, op.obj(a) == op.obj(b)), z3.And(
+                    ar(a, b),
+                    op.ival(a) == op.oval(b), # values must match
+                    z3.ForAll([a, b, c], z3.Implies(z3.And(op.type(c) == rd, wi(a, b)), z3.Not(wi(a, c)))), # at most one write a can write-into a read b
+                )),
                 # acyclicity
                 z3.ForAll([a, b], z3.Implies(wi(a, b), z3.Not(wi(b, a)))),
             ))
         )
 
-        so = H.Relation.session_order()
-        ar = AE.Relation.arbitration()
-        vis = AE.Relation.visibility()
-
-        # PRAM + wi
-        return z3.ForAll([a, b],
+        # session causality (enforced by WRF) + writes-into
+        return compose(WritesFollowReads.assertions(), z3.ForAll([a, b],
             z3.Implies(
-                # may be incorrect?
                 so(a, b),
                 z3.And(wi(a, b), vis(a, b), ar(a, b))
             )
-        )
+        ))

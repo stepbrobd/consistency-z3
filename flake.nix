01eeae880a7a8ab4f22a390dcce03ec05a0cda1e
Yifei Sun <ysun@hey.com>
2024-12-08 20:32:43 -0500

    devshell and formatter pkgs should track unstable

--- flake.nix
+++ flake.nix
@@ -1,20 +1,23 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/eabe8d3eface69f5bb16c18f8662a702f50c20d5";
+    unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
     pyproject.url = "github:nix-community/pyproject.nix";
     pyproject.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs @ { self, nixpkgs, parts, systems, pyproject }: parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ { self, parts, pyproject, ... }: parts.lib.mkFlake { inherit inputs; } {
     systems = import inputs.systems;
 
-    perSystem = { pkgs, ... }:
+    perSystem = { system, pkgs, unstable, ... }:
       let
         python = pkgs.python311;
       in
       {
+        _module.args.unstable = import inputs.unstable { inherit system; };
+
         packages.default = (python.pkgs.buildPythonPackage (
           (pyproject.lib.project.loadPyproject {
             projectRoot = ./.;
@@ -25,8 +28,8 @@
           pythonImportsCheck = [ "consistency" ];
         });
 
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
+        devShells.default = unstable.mkShell {
+          packages = with unstable; [
             direnv
             git
             hayagriva
@@ -47,13 +50,13 @@
           '';
         };
 
-        formatter = pkgs.writeShellScriptBin "formatter" ''
+        formatter = unstable.writeShellScriptBin "formatter" ''
           set -x
           shopt -s globstar
-          ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt .
-          ${pkgs.mypy}/bin/mypy --disable-error-code=import .
-          ${pkgs.ruff}/bin/ruff --fix --unsafe-fixes .
-          ${pkgs.typstfmt}/bin/typstfmt **/*.typ
+          ${unstable.nixpkgs-fmt}/bin/nixpkgs-fmt .
+          ${unstable.mypy}/bin/mypy --disable-error-code=import .
+          ${unstable.ruff}/bin/ruff check --fix --unsafe-fixes .
+          ${unstable.typstfmt}/bin/typstfmt **/*.typ
         '';
       };
 

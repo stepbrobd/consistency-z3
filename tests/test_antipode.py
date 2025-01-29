4ad724c429154ceee21d13f91fa7fe39685dfa00
Yifei Sun <ysun@hey.com>
2025-01-29 12:11:39 -0500

    WIP: antipode lineage dag constraints

--- tests/test_antipode.py
+++ tests/test_antipode.py
@@ -12,10 +12,11 @@ def test_antipode() -> None:
     op = Op.Create()
     a, b, x, y, z = Op.Consts("a b x y z")
 
+    hb = AE.Relation.happens_before()
     ar = AE.Relation.arbitration()
-    H.Relation.same_object()
-    H.Relation.returns_before()
-    AE.Relation.visibility()
+    so = H.Relation.same_object()
+    rb = H.Relation.returns_before()
+    vis = AE.Relation.visibility()
 
     # FIXME: temporary scaffolding
     # review lineage and xcy defs
@@ -25,32 +26,33 @@ def test_antipode() -> None:
             def lineage() -> z3.FuncDeclRef:
                 lineage = AE.Relation.Declare("lineage", op, op, z3.BoolSort())
 
-                Antipode.Relation.AddConstraint("lineage",
-                    z3.Implies(
-                        lineage(a, b),
-                        z3.And(  # type: ignore
-                            # 1. single root
-                            # 2. strict ordering within the same process of a lineage
-                            z3.If(op.proc(a) == op.proc(b), ar(a, b), z3.BoolVal(True)),  # type: ignore
-                            # 3. weak object/value equivalence
-                            z3.Or(
-                                op.obj(a) == op.obj(b),  # type: ignore
-                                op.ival(a) == op.oval(b),  # a:wr;b:rd # type: ignore
-                                op.oval(a) == op.oval(b),  # a:rd;b:rd # type: ignore
-                            ),
-                            # 4. invocation correspondence
-                            z3.BoolVal(True),
-                            # 5. reply correspondence
-                            z3.BoolVal(True),
-                            # *: transitivity
-                            z3.ForAll(
-                                x,
-                                z3.Implies(
-                                    z3.And(lineage(a, x), lineage(x, b)),
-                                    lineage(a, b),
-                                ),
+                Antipode.Relation.AddConstraint(
+                    "lineage",
+                    z3.And(  # type: ignore
+                        # 0. DAG: acyclicity and transitivity
+                        z3.ForAll(
+                            [a, b], z3.Implies(lineage(a, b), z3.Not(lineage(b, a)))
+                        ),
+                        z3.ForAll(
+                            [a, x, b],
+                            z3.Implies(
+                                z3.And(lineage(a, x), lineage(x, b)),
+                                lineage(a, b),
                             ),
                         ),
+                        # 1. single root
+                        # 2. strict ordering within the same process of a lineage
+                        z3.If(op.proc(a) == op.proc(b), ar(a, b), z3.BoolVal(True)),  # type: ignore
+                        # 3. weak object/value equivalence
+                        z3.Or(
+                            op.obj(a) == op.obj(b),  # type: ignore
+                            op.ival(a) == op.oval(b),  # a:wr; b:rd # type: ignore
+                            op.oval(a) == op.oval(b),  # a:rd; b:rd # type: ignore
+                        ),
+                        # 4. invocation correspondence
+                        z3.BoolVal(True),
+                        # 5. reply correspondence
+                        z3.BoolVal(True),
                     ),
                 )
 
@@ -74,10 +76,14 @@ def test_antipode() -> None:
                             # https://github.com/Antipode-SOSP23/antipode-deathstarbench
                             # https://github.com/Antipode-SOSP23/antipode-post-notification/blob/892a8c16922b1f4ec60dc6700f1ffca8629d5fc7/lambdas/reader.py#L63
                             z3.If(
-                                z3.And(op.type(y) == wr, op.type(b) == rd),  # TODO # type: ignore
+                                z3.And(
+                                    op.type(y) == wr, op.type(b) == rd
+                                ),  # TODO # type: ignore
                                 z3.And(
                                     lineage(a, b),
-                                    z3.ForAll([a], z3.BoolVal(True)),  # FIXME # type: ignore
+                                    z3.ForAll(
+                                        [a], z3.BoolVal(True)
+                                    ),  # FIXME # type: ignore
                                 ),
                                 z3.BoolVal(False),
                             ),

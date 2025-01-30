f240d476e2cc68476b392d3300b59f8064575d62
Yifei Sun <ysun@hey.com>
2025-01-30 15:09:14 -0500

    allow extensions in op declaration

--- consistency/operation.py
+++ consistency/operation.py
@@ -6,20 +6,21 @@ class Operation:
     _op_type = None
     _op_symbols: dict[str, z3.DatatypeRef] | None = None
 
+
     @staticmethod
-    def Create() -> z3.DatatypeSortRef:
+    def Create(extensions: list[tuple[str, z3.SortRef]] | None = None) -> z3.DatatypeSortRef:
         """
-        op = Operation.Create()
+        op = Operation.Create()  # default fields only
+        op = Operation.Create([("svc", z3.IntSort())])  # with additional fields
         """
         if Operation._op is not None:
-            return Operation._op
+            return Operation._op # type: ignore
 
         OperationType, (_, _) = Operation.Sort()
         Operation._op = z3.Datatype("Operation")
-        # nil constructor must not be declared
-        # Operation.declare("nil")
-        Operation._op.declare(
-            "cons",
+
+        # always include base declarations
+        base = [
             ("proc", z3.IntSort()),  # process id
             ("type", OperationType),  # operation type
             ("obj", z3.IntSort()),  # invoking object
@@ -27,10 +28,24 @@ class Operation:
             ("oval", z3.StringSort()),  # output value
             ("stime", z3.IntSort()),  # start time
             ("rtime", z3.IntSort()),  # return time
-        )
+        ]
+
+        if extensions is not None:
+            # check extension field names don't conflict with base fields
+            base_names = {field[0] for field in base}
+            for extension_name, _ in extensions:
+                if extension_name in base_names:
+                    raise ValueError(f"extension name `{extension_name}` conflicts with base fields")
+
+            all_fields = base + extensions
+        else:
+            all_fields = base
+
+        Operation._op.declare("cons", *all_fields)
         Operation._op = Operation._op.create()
 
         return Operation._op
+
 
     @staticmethod
     def Sort() -> tuple[z3.DatatypeSortRef, tuple[z3.DatatypeRef, ...]]:
@@ -43,6 +58,7 @@ class Operation:
         Operation._op_type = z3.EnumSort("OperationType", ["rd", "wr"])
 
         return Operation._op_type  # type: ignore
+
 
     @staticmethod
     def Const(name: str) -> z3.DatatypeRef:
@@ -59,6 +75,7 @@ class Operation:
         Operation._op_symbols[name] = z3.Const(name, op)  # type: ignore
 
         return Operation._op_symbols[name]
+
 
     @staticmethod
     def Consts(names: str) -> tuple[z3.DatatypeRef, ...]:

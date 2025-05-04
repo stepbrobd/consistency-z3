5fdf309a56cd1b8c8c8a69cd896606083f6b6bde
Yifei Sun <ysun@hey.com>
2025-05-04 17:53:08 -0400

    [wip] move xc to loop based extraction

--- tests/test_cross_causal.py
+++ tests/test_cross_causal.py
@@ -69,124 +69,67 @@ def test_cross_causal() -> None:
         assertion_func, symbol_gen = semantics[name]
         return assertion_func(symbol_gen(sys))
 
+
     # composition tree
     # [(name, prov, [to be added on edge])]
-    # ct = [
-    #     ("svc1", "mr", ["mw", "ryw", "wfr"]),
-    #     ("svc2", "mw", ["mr", "ryw", "wfr"]),
-    #     ("svc3", "ryw", ["mr", "mw", "wfr"]),
-    #     ("svc4", "wfr", ["mr", "mw", "ryw"]),
-    #     ("xc", None, None)
-    # ]
-
-    svc1 = Node(
-        name="SVC1",
-        needs=None,
-        provs=[(Cons("MR", assertion_for("mr", str(counter["mr"]))),)],
-    )
-    counter["mr"] += 1
-
-    # SVC1 -> SVC2
-    edge1_constraints = [
-        assertion_for("mr", str(counter["mr"])),
-        assertion_for("ryw", str(counter["ryw"])),
-        assertion_for("wfr", str(counter["wfr"])),
+    ct = [
+        ("svc1", "mr", ["mw", "ryw", "wfr"]),
+        ("svc2", "mw", ["mr", "ryw", "wfr"]),
+        ("svc3", "ryw", ["mr", "mw", "wfr"]),
+        ("svc4", "wfr", ["mr", "mw", "ryw"]),
+        ("xc", None, None)
     ]
-    counter["mr"] += 1
-    counter["ryw"] += 1
-    counter["wfr"] += 1
 
-    svc2 = Node(
-        name="SVC2",
-        needs=None,
-        provs=[(Cons("MW", assertion_for("mw", str(counter["mw"]))),)],
-    )
-    counter["mw"] += 1
+    nodes = []
+    edges = []
 
-    edge1 = Edge(
-        src=svc1,
-        dst=svc2,
-        cons=[(Cons("EC1", compose(*edge1_constraints)),)],
-    )
+    # create all nodes
+    for _i, (name, prov, _) in enumerate(ct):
+        node_name = name.upper()
 
-    # SVC2 -> SVC3
-    edge2_constraints = [
-        assertion_for("mr", str(counter["mr"])),
-        assertion_for("mw", str(counter["mw"])),
-        assertion_for("wfr", str(counter["wfr"])),
-    ]
-    counter["mr"] += 1
-    counter["mw"] += 1
-    counter["wfr"] += 1
+        if prov:
+            # node with provs
+            node = Node(
+                name=node_name,
+                needs=None,
+                provs=[(Cons(prov.upper(), assertion_for(prov, str(counter[prov]))),)]
+            )
+            counter[prov] += 1
+        else:
+            # xc node dont prov anything
+            node = Node(
+                name=node_name,
+                needs=None,
+                provs=None
+            )
 
-    svc3 = Node(
-        name="SVC3",
-        needs=None,
-        provs=[(Cons("RYW", assertion_for("ryw", str(counter["ryw"]))),)],
-    )
-    counter["ryw"] += 1
+        nodes.append(node)
 
-    edge2 = Edge(
-        src=svc2,
-        dst=svc3,
-        cons=[(Cons("EC2", compose(*edge2_constraints)),)],
-    )
+    # edges between nodes
+    for i in range(len(ct) - 1):
+        src_node = nodes[i]
+        dst_node = nodes[i+1]
+        _, _, edge_semantics = ct[i]
 
-    # SVC3 -> SVC4
-    edge3_constraints = [
-        assertion_for("mr", str(counter["mr"])),
-        assertion_for("mw", str(counter["mw"])),
-        assertion_for("ryw", str(counter["ryw"])),
-    ]
-    counter["mr"] += 1
-    counter["mw"] += 1
-    counter["ryw"] += 1
+        edge_constraints = []
+        for sem in edge_semantics:
+            edge_constraints.append(assertion_for(sem, str(counter[sem])))
+            counter[sem] += 1
 
-    svc4 = Node(
-        name="SVC4",
-        needs=None,
-        provs=[(Cons("WFR", assertion_for("wfr", str(counter["wfr"]))),)],
-    )
-    counter["wfr"] += 1
+        edge = Edge(
+            src=src_node,
+            dst=dst_node,
+            cons=[(Cons(f"EC{i+1}", compose(*edge_constraints)),)]
+        )
 
-    edge3 = Edge(
-        src=svc3,
-        dst=svc4,
-        cons=[(Cons("EC3", compose(*edge3_constraints)),)],
-    )
-
-    # SVC4 -> XC
-    edge4_constraints = [
-        assertion_for("mr", str(counter["mr"])),
-        assertion_for("mw", str(counter["mw"])),
-        assertion_for("ryw", str(counter["ryw"])),
-    ]
-    counter["mr"] += 1
-    counter["mw"] += 1
-    counter["ryw"] += 1
-
-    xc = Node(
-        name="XC",
-        needs=None,
-        provs=None,
-    )
-
-    edge4 = Edge(
-        src=svc4,
-        dst=xc,
-        cons=[(Cons("EC4", compose(*edge4_constraints)),)],
-    )
-
-    # full graph
-    nodes = [svc1, svc2, svc3, svc4, xc]
-    edges = [edge1, edge2, edge3, edge4]
+        edges.append(edge)
 
     g = graph(nodes, edges)
-    ok, res = composable(g, svc1)
+    ok, res = composable(g, nodes[0])
     # assert ok
 
     # sample extraction for further use
-    aggcons = extract(svc1, svc1, (ok, res))
+    aggcons = extract(nodes[0], nodes[0], (ok, res))
     Node(
         name="XC",
         needs=None,
